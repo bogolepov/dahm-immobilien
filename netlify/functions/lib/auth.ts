0e65b939bc2d7bee259c1906b071fec79b3749d1
bogolepov <bogolepov@gmail.com>
2026-06-02 05:41:24 +0200

    JWT Secret added

--- netlify/functions/lib/auth.ts
+++ netlify/functions/lib/auth.ts
@@ -1,5 +1,6 @@
 import type { UserRole } from '@scripts/zod';
 import jwt from 'jsonwebtoken';
+import dahm from '@data/dahm.json';
 
 export const JWT_EXPIRES_IN__TEXT = '8h';
 export const JWT_EXPIRES_IN__NUMBER = 8 * 60 * 60; // in seconds!
@@ -8,14 +9,24 @@ export interface JwtPayload {
 	role: UserRole;
 }
 
-const JWT_SECRET = process.env.JWT_SECRET!;
+const JwtOptions: jwt.SignOptions = {
+	algorithm: 'ES256' as const,
+	issuer: dahm.domain,
+	audience: dahm.domain,
+};
 
 export function createToken(payload: JwtPayload) {
-	return jwt.sign(payload, JWT_SECRET, {
+	return jwt.sign(payload, process.env.JWT_ACCESS_PRIVATE_KEY as jwt.Secret, {
+		...JwtOptions,
 		expiresIn: JWT_EXPIRES_IN__TEXT,
 	});
 }
 
 export function verifyToken(token: string) {
-	return jwt.verify(token, JWT_SECRET) as JwtPayload;
+	try {
+		return jwt.verify(token, process.env.JWT_ACCESS_PUBLIC_KEY as jwt.Secret) as JwtPayload;
+	} catch (error) {
+		if (error instanceof jwt.TokenExpiredError) return undefined;
+		throw error;
+	}
 }
